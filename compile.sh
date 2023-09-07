8ffd7b82c838678aa1a0141c9278076456dc02fa
Brett Kuskie <fullaxx@gmail.com>
2023-09-07 03:42:45 -0600

    Add structured version of mt32 with unittest

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@
 
 set -e
 
+gcc -Wall -ggdb3 main_mt32.c mt32.c -o mt32.dbg
 gcc -Wall -ggdb3 main_mt64.c mt64.c -o mt64.dbg
--- unittest.sh
+++ unittest.sh
@@ -2,4 +2,5 @@
 
 set -e
 
+./mt32.dbg | sha256sum | grep -q be6d89fbf6ffdd1b67ce830abda146b53a55a55e8fb2f0038413acee5779f683
 ./mt64.dbg | sha256sum | grep -q 00d549465bf1edddd2dffdcc61703da18116f676ec19599238c1912cc9155320
